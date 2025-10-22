65731bcbadeb4f409d98ad7e91661dfe22739120
Marques Kevin <marques.developpement@gmail.com>
2025-10-22 15:11:17 +0200

    feat: add logo on navbar (#4)

---
+++ src/modules/global/components/global_logo/global_logo.stories.tsx
@@ -0,0 +1,12 @@
+import { GlobalLogo } from "./global_logo"
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta: Meta<typeof GlobalLogo> = {
+  title: "Global/global_logo",
+  component: GlobalLogo,
+}
+
+export default meta
+type Story = StoryObj<typeof GlobalLogo>
+
+export const Default: Story = {}
--- src/modules/global/components/global_navbar/global_navbar.tsx
+++ src/modules/global/components/global_navbar/global_navbar.tsx
@@ -1,6 +1,7 @@
 import { useIntl } from "react-intl"
 import { Link, NavLink } from "react-router-dom"
 import { Cog, Plus } from "lucide-react"
+import { GlobalLogo } from "../global_logo/global_logo"
 
 export function GlobalNavbar() {
   const { formatMessage } = useIntl()
@@ -9,8 +10,18 @@ export function GlobalNavbar() {
     <nav className="text-base-content container mx-auto">
       <div className="flex items-center justify-between gap-4 py-8">
         <Link to="/" className="flex items-center gap-2">
-          <span className="text-lg font-semibold tracking-tight">
-            Tinycardo
+          <GlobalLogo className="size-10" />
+
+          <span
+            className="text-primary text-lg font-semibold tracking-wider"
+            style={{
+              textDecoration: "underline",
+              textDecorationThickness: "2px",
+              textDecorationColor: "var(--color-primary-content)",
+              textUnderlineOffset: "2px",
+            }}
+          >
+            tinycardo
           </span>
         </Link>
 
