ba67d06996326565a02d2d84cf099a263ec58a3b
Kevin Marques <marques.developpement@gmail.com>
2025-10-22 10:27:13 +0200

    save

---
+++ src/modules/global/components/global_footer/global_footer.container.ts
@@ -0,0 +1,20 @@
+import { THEMES } from "@/modules/params/constants/themes"
+import { store_theme } from "@/modules/params/redux/params_actions"
+import { type Dispatch, type RootState } from "@/redux/store"
+import { connect, type ConnectedProps } from "react-redux"
+
+const map_state = (state: RootState) => {
+  return {
+    themes: THEMES,
+    selected_theme: state.params.theme,
+  }
+}
+
+const map_dispatch = (dispatch: Dispatch) => ({
+  on_change_theme(theme: string) {
+    dispatch(store_theme({ theme }))
+  },
+})
+
+export const connector = connect(map_state, map_dispatch)
+export type ContainerProps = ConnectedProps<typeof connector>
